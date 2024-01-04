159faa40bf83ff49f3e31d66065dce914f3c1c6f
Alexey-Voronin <voronin2@illinois.edu>
2024-01-04 10:58:53 -0800

    recollect result for different param choices

---
+++ data_collection/parameter_search/th/lsc_dc_new/eta_ho/collect.py
@@ -0,0 +1,57 @@
+##########################################################
+# Data collection script
+##########################################################
+# Infer Problem type from the path
+# e.g. `python  collect.py structured/3D` command
+# results in msh_type='structured' and dim=3
+##########################################################
+import sys, os
+
+path = sys.argv[1]
+msh_type, dim_str = path.split("/")
+dim = int(dim_str[0])
+##########################################################
+# Generate Problem Iterator
+##########################################################
+from problem_iterator import get_problem_iterator
+
+iterator = get_problem_iterator(msh_type, dim)
+disc_type = iterator.system_params["discretization"]["elem_type"]
+##########################################################
+# Generate MG parameters
+# 'hlo' : $M_{ph}^{\omega_i>0}$
+# 'ho'  : $M_{ph}^{\omega_1=0}$
+# 'lo'  : $M_{ph}^{\omega_0=0}$
+##########################################################
+sys.path.append(os.path.abspath("../"))
+from mg_params import get_mg_params
+
+mg_type = "ho"
+mg_params = get_mg_params(msh_type, disc_type, dim, mg_type)
+##########################################################
+# Switch to directory where the files will be wrriten to.
+##########################################################
+os.chdir(os.path.join(os.getcwd(), path))
+##########################################################
+# Collect convergence data
+##########################################################
+from sysmg.util.param_search import parameter_scan
+import numpy as np
+
+
+def setter(amg, params):
+    # amg.wrapper.set_eta(((1,eta0_p), (1,1)))
+    # amg.wrapper.set_eta(params)
+    amg.wrapper.set_eta(((params[0], params[0]), (1, 1)))
+
+
+dp = 3  # digits past decimal place
+tau_range = np.round(np.linspace(0.3, 1.3, 70), dp)
+param_search = {
+    "\eta": (tau_range,),
+}
+
+from sysmg import StokesMG
+
+parameter_scan(iterator, StokesMG, mg_params, setter, param_search)
+##########################################################
